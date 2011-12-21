eb609bbe7a1d2581d925d256168687de9376e178
Edson Hilios <ehilios@corp.hotwords.com>
2011-12-21 15:04:13 -0200

    Just delegating the setting of the middleware to sinatra.rb

--- config.ru
+++ config.ru
@@ -1,8 +1,6 @@
 # The call of run method is inside the required file
 require ::File.expand_path('../sinatra',  __FILE__)
 
-use Rack::ShowExceptions
-
 map '/assets' do
   run settings.sprockets
 end
--- sinatra.rb
+++ sinatra.rb
@@ -2,20 +2,23 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sprockets'
 require 'sinatra'
-# Some setups
+# Environment
 ENV['RACK_ENV'] ||= :development
-
+# Some settings
 set :root,  File.expand_path('../', __FILE__)
-set :views, File.join(settings.root, 'app', 'views')
-
+set :views, 'app/views'
 set :haml, :format => :html5
-
-# Load all files from my app
-Dir["./app/**/*.rb"].each { |f| require f }
-
+# Sessions
+enable :sessions
+set :session_secret, '1Gikx4OTdoQp9OLjxfK76NBm065IzPkYTAirE8iUT5wgXAIW30dbjxOr5riSvRrKEQ7JxDsk7Kfz363Vif2erbgSZt3Xjh6hs8ZX8cO6X0ntzYYhgYzUmedQG8WielBh'
 # Configure sprockets
 sprockets = Sprockets::Environment.new
-sprockets.append_path 'assets/javascripts'
-sprockets.append_path 'assets/stylesheets'
-sprockets.append_path 'assets/images'
+sprockets.append_path 'app/assets/javascripts'
+sprockets.append_path 'app/assets/stylesheets'
+sprockets.append_path 'app/assets/images'
 set :sprockets, sprockets
+# Middlewares
+use Rack::Session::Pool, :expire_after => 2592000
+use Rack::ShowExceptions
+# Load all files from my app
+Dir["./app/**/*.rb"].each { |f| require f }
