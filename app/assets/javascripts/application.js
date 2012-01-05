ef629905fd3cd86eb0f051898a1336771e683655
Edson Hilios <ehilios@corp.hotwords.com>
2012-01-05 11:15:35 -0200

    Correcting the path to three.js on app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require vendor/jquery-1.7.1.min.js
 //= require vendor/jquery-ui-1.8.16.min.js
 //= require vendor/jquery.gracefulWebSocket.js
-//= require three/custom/ThreeCanvas.js
-//= require three/custom/ThreeExtras.js
+//= require vendor/three/custom/ThreeCanvas.js
+//= require vendor/three/custom/ThreeExtras.js
 //= require_tree .
