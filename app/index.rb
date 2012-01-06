92b887300f37541ad852d0588b235e3a4ceda125
Edson Hilios <edson.hilios@gmail.com>
2012-01-06 08:50:30 -0200

    Going ahead with the layout a little bit more

--- app/index.rb
+++ app/index.rb
@@ -5,3 +5,23 @@ end
 get '/' do
   haml :index
 end
+
+get '/blog' do
+  redirect 'http://hilios.tumblr.com'
+end
+
+get '/linkedin' do
+  redirect 'http://www.linkedin.com/in/edsonhilios'
+end
+
+get '/github' do
+  redirect 'https://github.com/hilios'
+end
+
+get '/twitter' do
+  redirect 'http://twitter.com/hilios'
+end
+
+get '/facebook' do
+  redirect 'http://www.facebook.com/hilios'
+end
