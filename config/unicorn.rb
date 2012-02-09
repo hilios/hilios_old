a66c75f6a3b1e2782d2317a65964d15fb737baff
Edson Hilios <ehilios@corp.hotwords.com>
2012-02-09 14:08:54 -0200

    Adjusting the correct pids folder on shared resources

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,7 +20,7 @@ timeout 30
 working_directory "#{root_path}/current"
 stderr_path       "#{shared_path}/log/error.log"
 stdout_path       "#{shared_path}/log/production.log"
-pid               "#{shared_path}/tmp/pids/unicorn.pid"
+pid               "#{shared_path}/pids/unicorn.pid"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 preload_app true
