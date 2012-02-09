a3f576760b971fb1211a508a5646061f642a82cc
Edson Hilios <ehilios@corp.hotwords.com>
2012-02-09 14:58:28 -0200

    Correcting a typo on the socket path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ worker_processes 4
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "#{shared_path}/tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{shared_path}/sockets/unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
