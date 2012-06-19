fa5e91e13d9e22129b838366ec064da7c70fbd2f
Edson Hilios <ehilios@corp.hotwords.com>
2012-06-19 15:01:14 -0300

    Corrected path to the app

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,4 @@
-app_path = "/var/rails/hilios"
+app_path = "/var/ruby/hilios"
 
 timeout 30
 worker_processes 1
@@ -16,6 +16,24 @@ GC.respond_to?(:copy_on_write_friendly=) and
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
+  
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+  old_pid = "#{app_path}/shared/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
 end
 
 after_fork do |server, worker|
